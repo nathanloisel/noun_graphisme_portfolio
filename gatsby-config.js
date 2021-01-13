bca2b073319798f8ef8a7d119f43361fde607c4e
Nathan Loisel <nathan.loisel@outlook.com>
2021-01-13 11:20:12 +0100

    new favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -97,48 +97,6 @@ module.exports = {
         icon: 'src/images/icon-512x512.png',
         icons: [
           {
-            src: 'src/images/icon-48x48.png',
-            type: 'image/png',
-            sizes: '48x48',
-            purpose: 'any maskable',
-          },
-          {
-            src: 'src/images/icon-72x72.png',
-            type: 'image/png',
-            sizes: '72x72',
-            purpose: 'any maskable',
-          },
-          {
-            src: 'src/images/icon-96x96.png',
-            type: 'image/png',
-            sizes: '96x96',
-            purpose: 'any maskable',
-          },
-          {
-            src: 'src/images/icon-144x144.png',
-            type: 'image/png',
-            sizes: '144x144',
-            purpose: 'any maskable',
-          },
-          {
-            src: 'src/images/icon-192x192.png',
-            type: 'image/png',
-            sizes: '192x192',
-            purpose: 'any maskable',
-          },
-          {
-            src: 'src/images/icon-256x256.png',
-            type: 'image/png',
-            sizes: '256x256',
-            purpose: 'any maskable',
-          },
-          {
-            src: 'src/images/icon-384x384.png',
-            type: 'image/png',
-            sizes: '384x384',
-            purpose: 'any maskable',
-          },
-          {
             src: 'src/images/icon-512x512.png',
             type: 'image/png',
             sizes: '512x512',
